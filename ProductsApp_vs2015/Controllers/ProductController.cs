5ddd5ee4b9948d34def20befb7a50637ccb694ba
venupigilam <venupigilam@gmail.com>
2018-02-03 18:29:59 +0530

    Test Commit

--- ProductsApp_vs2015/Controllers/ProductController.cs
+++ ProductsApp_vs2015/Controllers/ProductController.cs
@@ -14,7 +14,8 @@ namespace ProductsApp_vs2015.Controllers
        {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Bat", Category = "Wood", Price = 106.59M }
        };
 
         public IEnumerable<Product> GetAllProducts()
